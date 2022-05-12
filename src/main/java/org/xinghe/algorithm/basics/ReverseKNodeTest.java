e4903d97522d6762dd1cf71a4a2769e274f7bbfa
星河 <hechunrun@foxmail.com>
2022-05-12 22:49:32 +0800

    单链表分组反转：待实现

---
+++ src/main/java/org/xinghe/algorithm/basics/ReverseKNodeTest.java
@@ -0,0 +1,34 @@
+package org.xinghe.algorithm.basics;
+
+/**
+ * 描述 https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+ *
+ * @author 星河君❀❀ 2022/5/12 22:41
+ * @see [相关类/方法]
+ * @since [版本号]
+ */
+public class ReverseKNodeTest {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k){
+        for (int i = 0; i < k; i++) {
+
+        }
+        return new ListNode();
+    }
+}
