1ad47cb3ca3948a773ce315908944acab881cd71
星河 <xinghejun@foxmail.com>
2022-05-18 19:25:48 +0800

    添加剑指offer的官方实现

--- src/main/java/org/xinghe/algorithm/sword/charpter4/Test27.java
+++ src/main/java/org/xinghe/algorithm/sword/charpter4/Test27.java
@@ -27,17 +27,17 @@ public class Test27 {
         dummy.next = head;
         ListNode fast = dummy;
         ListNode slow = dummy;
-        while (slow != null && fast.next != null){
+        while (slow != null && fast.next != null) {
             fast = fast.next;
             slow = slow.next;
-            if (fast.next != null){
+            if (fast.next != null) {
                 fast = fast.next;
             }
         }
         ListNode postHalfListNode = slow.next;
         ListNode reverse = reverse(postHalfListNode);
-        while (reverse != null && head != null){
-            if (reverse.val != head.val){
+        while (reverse != null && head != null) {
+            if (reverse.val != head.val) {
                 return false;
             }
             reverse = reverse.next;
@@ -47,10 +47,45 @@ public class Test27 {
         return true;
     }
 
-    private ListNode reverse(ListNode head){
+    public boolean isPalindrome2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        if (head.next == null) {
+            return true;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        ListNode postHalfList = slow.next;
+        if (fast.next != null) {
+            postHalfList = slow.next.next;
+        }
+        slow.next = null;
+
+        return equals(postHalfList, reverse(head));
+    }
+
+    private boolean equals(ListNode head1, ListNode head2) {
+        while (head1 != null && head2 != null) {
+            if (head1.val != head2.val) {
+                return false;
+            }
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        return head1 == null && head2 == null;
+    }
+
+    private ListNode reverse(ListNode head) {
         ListNode prev = null;
         ListNode current = head;
-        while (current != null){
+        while (current != null) {
             ListNode next = current.next;
             current.next = prev;
             prev = current;
@@ -63,16 +98,16 @@ public class Test27 {
         ListNode one = new ListNode(1);
         one.next = new ListNode(2);
         one.next.next = new ListNode(3);
-        System.out.println(new Test27().isPalindrome(one));
+        System.out.println(new Test27().isPalindrome2(one));
 
         ListNode two = new ListNode(1);
         two.next = new ListNode(2);
         two.next.next = new ListNode(3);
         two.next.next.next = new ListNode(2);
         two.next.next.next.next = new ListNode(1);
-        System.out.println(new Test27().isPalindrome(two));
+        System.out.println(new Test27().isPalindrome2(two));
 
         ListNode three = new ListNode(3);
-        System.out.println(new Test27().isPalindrome(three));
+        System.out.println(new Test27().isPalindrome2(three));
     }
 }
