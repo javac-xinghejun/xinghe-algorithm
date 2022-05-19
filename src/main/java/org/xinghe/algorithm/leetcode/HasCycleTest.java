f92431a03f4c7e92df5f7344d5eebe8588ca87a3
星河 <xinghejun@foxmail.com>
2022-05-19 17:35:10 +0800

    Create HasCycleTest.java

---
+++ src/main/java/org/xinghe/algorithm/leetcode/HasCycleTest.java
@@ -0,0 +1,29 @@
+package org.xinghe.algorithm.leetcode;
+
+import org.xinghe.algorithm.basics.ListNode;
+
+/**
+ * https://leetcode.cn/problems/linked-list-cycle/
+ *
+ * @author 星河君❀❀ 2022/5/9 20:10
+ * @see [相关类/方法]
+ * @since [版本号]
+ */
+public class HasCycleTest {
+    public static boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (fast != slow) {
+
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
